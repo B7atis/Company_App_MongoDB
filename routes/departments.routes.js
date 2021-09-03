42bff313e9f32186ed092273b0c23101fe8b7f81
Bartosz Jędrzejewski-Emche <jedrek2603@gmail.com>
2021-09-03 22:22:32 +0200

    Add Mongoose in file employees.routes.js and products.routes.js

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -12,7 +12,6 @@ router.get('/departments', async (req, res) => {
 });
 
 router.get('/departments/random', async (req, res) => {
-
   try {
     const count = await Department.countDocuments();
     const rand = Math.floor(Math.random() * count);
@@ -23,11 +22,9 @@ router.get('/departments/random', async (req, res) => {
   catch (err) {
     res.status(500).json({ message: err });
   }
-
 });
 
 router.get('/departments/:id', async (req, res) => {
-
   try {
     const dep = await Department.findById(req.params.id);
     if (!dep) res.status(404).json({ message: 'Not found' });
@@ -36,27 +33,21 @@ router.get('/departments/:id', async (req, res) => {
   catch (err) {
     res.status(500).json({ message: err });
   }
-
 });
 
 router.post('/departments', async (req, res) => {  //korzystając z metody async...await - przechowujemy kod w bloku try..catch - pozwala to wyłapywać błędy
-
   try {
-
     const { name } = req.body;  // wyciąga parametr name z req.body
     const newDepartment = new Department({ name: name });  // tworzy nowy dokument na bazie modelu Department
     await newDepartment.save();  // save = zapisz dokument w kolekcji
     res.json({ message: 'OK' });  // oczekuje na wykonanie metody (await) i jeśli jest dobrze to zwraca komunikat OK
-
   } catch (err) {
     res.status(500).json({ message: err });
   }
-
 });
 
 router.put('/departments/:id', async (req, res) => {
   const { name } = req.body;
-
   try {
     const dep = await Department.findById(req.params.id);
     if (dep) {
@@ -68,11 +59,9 @@ router.put('/departments/:id', async (req, res) => {
   catch (err) {
     res.status(500).json({ message: err });
   }
-
 });
 
 router.delete('/departments/:id', async (req, res) => {
-
   try {
     const dep = await Department.findById(req.params.id);
     if (dep) {
@@ -84,7 +73,6 @@ router.delete('/departments/:id', async (req, res) => {
   catch (err) {
     res.status(500).json({ message: err });
   }
-
 });
 
 module.exports = router;
