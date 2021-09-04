6c5453570634e6183863aba1f170e20bb7ea937d
Bartosz Jędrzejewski-Emche <jedrek2603@gmail.com>
2021-09-04 22:22:48 +0200

    Add a populate() method. Extract functions from all route.js files and move them to separate files in the controllers folder.

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -1,79 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const Department = require('../models/department.model');
+const DepartmentController = require('../controllers/departments.controller');
 
-router.get('/departments', async (req, res) => {
-  try {
-    res.json(await Department.find());
-  }
-  catch (err) {
-    res.status(500).json({ message: err });
-  }
-});
+router.get('/departments', DepartmentController.getAll);
 
-router.get('/departments/random', async (req, res) => {
-  try {
-    const count = await Department.countDocuments();
-    const rand = Math.floor(Math.random() * count);
-    const dep = await Department.findOne().skip(rand);
-    if (!dep) res.status(404).json({ message: 'Not found' });
-    else res.json(dep);
-  }
-  catch (err) {
-    res.status(500).json({ message: err });
-  }
-});
+router.get('/departments/random', DepartmentController.getRandom);
 
-router.get('/departments/:id', async (req, res) => {
-  try {
-    const dep = await Department.findById(req.params.id);
-    if (!dep) res.status(404).json({ message: 'Not found' });
-    else res.json(dep);
-  }
-  catch (err) {
-    res.status(500).json({ message: err });
-  }
-});
+router.get('/departments/:id', DepartmentController.getById);
 
-router.post('/departments', async (req, res) => {  //korzystając z metody async...await - przechowujemy kod w bloku try..catch - pozwala to wyłapywać błędy
-  try {
-    const { name } = req.body;  // wyciąga parametr name z req.body
-    const newDepartment = new Department({ name: name });  // tworzy nowy dokument na bazie modelu Department
-    await newDepartment.save();  // save = zapisz dokument w kolekcji
-    res.json({ message: 'OK' });  // oczekuje na wykonanie metody (await) i jeśli jest dobrze to zwraca komunikat OK
-  } catch (err) {
-    res.status(500).json({ message: err });
-  }
-});
+router.post('/departments', DepartmentController.post);
 
-router.put('/departments/:id', async (req, res) => {
-  const { name } = req.body;
-  try {
-    const dep = await Department.findById(req.params.id);
-    if (dep) {
-      await Department.updateOne({ _id: req.params.id }, { $set: { name: name } });
-      const updatedDep = await Department.findById(req.params.id);
-      res.json({ message: 'OK', updatedDep });
-    }
-    else res.status(404).json({ message: 'Not found...' });
-  }
-  catch (err) {
-    res.status(500).json({ message: err });
-  }
-});
+router.put('/departments/:id', DepartmentController.update);
 
-router.delete('/departments/:id', async (req, res) => {
-  try {
-    const dep = await Department.findById(req.params.id);
-    if (dep) {
-      await Department.deleteOne({ _id: req.params.id });
-      res.json({ message: 'OK', deletedDep: dep });
-    }
-    else res.status(404).json({ message: 'Not found...' });
-  }
-  catch (err) {
-    res.status(500).json({ message: err });
-  }
-});
+router.delete('/departments/:id', DepartmentController.delete);
 
 module.exports = router;
